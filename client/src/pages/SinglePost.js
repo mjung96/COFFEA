d0dcef66b4a7b055fa3d88adddcb6d6358ce2918
Michael Jung <michaeljung1996@gmail.com>
2021-08-23 11:40:32 -0400

    Added navigation to SinglePost
    Can click on titles of posts on Home page to navigate to Specific Post Page. Still a bug when you try to like a post without being logged in...

--- client/src/pages/SinglePost.js
+++ client/src/pages/SinglePost.js
@@ -1,7 +1,8 @@
 import React, { useContext } from 'react';
+import { useParams } from 'react-router-dom';
 import gql from 'graphql-tag';
 import { useQuery, useMutation } from '@apollo/react-hooks';
-import { Card, Grid, Image, Button, Icon, Label, Form } from 'semantic-ui-react';
+import { Card, Grid, Image, Button, Icon, Label, Form, Item, GridColumn } from 'semantic-ui-react';
 import moment from 'moment';
 
 import { AuthContext } from '../context/auth';
@@ -9,79 +10,115 @@ import LikeButton from '../components/LikeButton';
 import DeleteButton from '../components/DeleteButton';
 
 function SinglePost(props) {
-    const postId = props.match.params.postId;
+    let postId = props.match.params.postId;
     const { user } = useContext(AuthContext);
 
+    //let postId = useParams(); 
 
-
-    const { data: { getPost }} = useQuery(FETCH_POST_QUERY, {
+    const { data } = useQuery(FETCH_POST_QUERY, {
         variables: {
             postId
         }
-    })
+    });
+
+    //const { data } = useQuery(FETCH_POST_QUERY)
 
     let postMarkup;
-    if(!getPost) {
+    if(!data) {
         postMarkup = <p>Loading...</p>
     }
     else {
-        const { id, body, createdAt, username, comments, likes, likeCount, commentCount, title, summary, imageUrl } = getPost;
+        const { id, body, createdAt, username, comments, likes, likeCount, commentCount, title, summary, imageUrl } = data.getPost;
         postMarkup = (
-            <Grid>
-                <Grid.Row>
-                    <Grid.Column width={2}>
-                        <Image
-                            src = {imageUrl}
-                            size = "massive"
-                            float = "right"
-                        />
-                    </Grid.Column>
-                    <Grid.Column width={10}>
-                        <Card fluid>
-                            <Card.Content> 
-                                <Card.Header>
-                                    {title}
-                                </Card.Header>
-                                <Card.Meta>
-                                    {username} 
-                                </Card.Meta>
-                                <Card.Meta>
-                                    {moment(createdAt).fromNow()}
-                                </Card.Meta>
-                                <Card.Description>
-                                    {body}
-                                </Card.Description>
-                            </Card.Content>
-                            <hr />
-                            <Card.Content extra>
-                                <LikeButton user={user} post={{id, likeCount, likes}}/>
-                                <Button
+
+            <Item.Group>
+                <Item>
+                    <Item.Image
+                        src = {imageUrl}
+                        size = "medium"
+                    />
+                    <Item.Content>
+                        <Item.Header>
+                            {title}
+                        </Item.Header>
+                        <Item.Meta>
+                            {username}, {moment(createdAt).fromNow()}
+                        </Item.Meta>
+                        <Item.Description>
+                            {body}
+                        </Item.Description>
+                        <Item.Extra>
+                        <LikeButton user={user} post={{id, likeCount, likes}}/>
+                                  <Button
                                     as="div"   
-                                    labelPosition="right" 
-                                    onClick={() => console.log('Comment on post')}
-                                    >
-                                        <Button basic color="black">
-                                            <Icon name="comment"/>
-                                        </Button>
-                                        <Label basic color="black" pointing="left">
-                                            {commentCount}
-                                        </Label>
-                                </Button>
-                                {user && user.username === username && (
-                                    <DeleteButton postId={id}/>
-                                )}
-                            </Card.Content>
-                        </Card>
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
+                                     labelPosition="right" 
+                                     onClick={() => console.log('Comment on post')}
+                                     >
+                                         <Button basic color="black">
+                                             <Icon name="comment"/>
+                                         </Button>
+                                         <Label basic color="black" pointing="left">
+                                             {commentCount}
+                                         </Label>
+                                 </Button>
+                                 {user && user.username === username && (
+                                     <DeleteButton postId={id}/>
+                                 )}
+                        </Item.Extra>
+                    </Item.Content>
+                </Item>
+            </Item.Group>
+
+            // <Grid centered>
+            //     <Grid.Row>
+            //         <Image
+            //             src = {imageUrl}
+            //             size = "large"
+            //         />
+            //     </Grid.Row>
+            //     <Grid.Row>
+            //         <Grid.Column>
+            //             <Card fluid>
+            //                 <Card.Content>
+            //                 <Card.Header>
+            //                     {title}
+            //                 </Card.Header>
+            //                 <Card.Meta>
+            //                     {username}, {moment(createdAt).fromNow()}
+            //                 </Card.Meta>
+            //                 <Card.Description>
+            //                     {body}
+            //                 </Card.Description>
+            //                 <Card.Content extra>
+            //                      <LikeButton user={user} post={{id, likeCount, likes}}/>
+            //                      <Button
+            //                          as="div"   
+            //                          labelPosition="right" 
+            //                          onClick={() => console.log('Comment on post')}
+            //                          >
+            //                              <Button basic color="black">
+            //                                  <Icon name="comment"/>
+            //                              </Button>
+            //                              <Label basic color="black" pointing="left">
+            //                                  {commentCount}
+            //                              </Label>
+            //                      </Button>
+            //                      {user && user.username === username && (
+            //                          <DeleteButton postId={id}/>
+            //                      )}
+            //                  </Card.Content>
+            //             </Card.Content>
+            //             </Card>
+            //         </Grid.Column>
+            //     </Grid.Row>
+            // </Grid>
         );
     }
     return postMarkup;
 }
 
 const FETCH_POST_QUERY = gql`
-    query($postId: ID!) {
+    query getPost($postId: ID!) {
         getPost(postId: $postId) {
             id
             body
